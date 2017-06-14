b3596b557b70d3dfb830051c2ea0466bb4f87550
Denis Tarasov <deolands@gmail.com>
2017-06-14 18:10:23 +0300

    edit .gitignore, add Book's model

---
+++ src/main/java/com/mrak/bookmanager/dao/BookDaoImpl.java
@@ -0,0 +1,4 @@
+package com.mrak.bookmanager.dao;
+
+public class BookDaoImpl {
+}
---
+++ src/main/java/com/mrak/bookmanager/model/Book.java
@@ -0,0 +1,63 @@
+package com.mrak.bookmanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "BOOK_TITLE")
+    private String bookTitlel;
+
+    @Column(name = "BOOK_AUTHOR")
+    private String bookAuthor;
+
+    @Column(name = "BOOK_PRICE")
+    private int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookTitlel() {
+        return bookTitlel;
+    }
+
+    public void setBookTitlel(String bookTitlel) {
+        this.bookTitlel = bookTitlel;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitlel=" + bookTitlel +
+                ", bookAuthor=" + bookAuthor +
+                ", price=" + price +
+                '}';
+    }
+}
